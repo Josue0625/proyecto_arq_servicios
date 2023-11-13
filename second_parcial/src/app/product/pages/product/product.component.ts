300402be6bc8b070c3f390ad55d00bacfac19920
Josue0625 <jcampos@ufpso.edu.co>
2023-11-12 21:56:06 -0500

    se arreglo el obtener usuario y el moverse a traves de rutas

--- second_parcial/src/app/product/pages/product/product.component.ts
+++ second_parcial/src/app/product/pages/product/product.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '../../../home/auth.service';
 
 @Component({
   selector: 'app-product',
@@ -8,6 +9,23 @@ import { CommonModule } from '@angular/common';
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit{
+
+  constructor(private ser: AuthService){}
+
+  ngOnInit(){
+    this.obtener();
+  }
+  obtener(){
+    this.ser.getUsuario().subscribe(res=>{
+
+      console.log("Ta bien");
+      console.log(res);
+    },
+    (err)=>{
+      console.log("error");
+      console.log(err);
+    })
+  } 
 
 }
